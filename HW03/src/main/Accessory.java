dc47676aeef6e6e220ffa85fd6413daf68e3903f
AndriiAV <Andrii.Andruschenko@gmail.com>
2017-07-27 14:20:06 +0300

    Addition enum

---
+++ HW03/src/main/Accessory.java
@@ -0,0 +1,8 @@
+package main;
+
+public enum Accessory {
+	BASE_OF_FLOWER,	//Основа цветка
+	WRAPPING,		//Обертка
+	RIBBON,			//Ленточка
+	BASKET			//Корзинка
+}
---
+++ HW03/src/main/Freshness.java
@@ -0,0 +1,8 @@
+package main;
+
+public enum Freshness {
+	VERY_FRESH,	//Очень свежий
+	NORMAL,		//Норм
+	BAD,		//Плохая
+	WILTED		//Завял
+}
